d4f46820382873203e669312e09947418c45c474
JestyUY <104555840+JestyUY@users.noreply.github.com>
2024-08-05 16:05:24 +1200

    file size changed.

--- app/api/middleware.ts
+++
@@ -1,8 +0,0 @@
-import nc from "next-connect";
-import bodyParser from "body-parser";
-
-const handler = nc()
-  .use(bodyParser.json({ limit: "10mb" }))
-  .use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
-
-export default handler;
